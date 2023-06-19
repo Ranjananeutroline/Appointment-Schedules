d0f29d3e3db190bae990d7b606fee0a3101b475b
Ranjana <Ranjana.neutroline@gmail.com>
2023-06-19 08:00:40 +0545

    19thjune-formvalidate

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -7,25 +7,21 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { BsTelephoneFill } from 'react-icons/bs';
 import DayTimePicker from '@mooncake-dev/react-day-time-picker';
+import TimezoneSelect, { allTimezones } from 'react-timezone-select'
 import spacetime from "spacetime";
 import Modal from 'react-bootstrap/Modal';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import { SelectTimezone } from "reactjs-timezone-select";
-import { ImEarth } from 'react-icons/im';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+
 
  function Details() {
     // const [value, onChange] = useState(new Date());
 
-    const [tz, setTz] = useState(
+    const [selectedTimezone, setSelectedTimezone] =useState(
         Intl.DateTimeFormat().resolvedOptions().timeZone
-      );
-      const [datetime, setDatetime] = useState(spacetime.now());
-      
-      useMemo(() => {
-        const tzValue = tz.value ?? tz;
-        setDatetime(datetime.goto(tzValue));
-      }, [tz]);
+      )
 
         const [date, setDate] = useState(new Date());
         
@@ -34,13 +30,23 @@ import { ImEarth } from 'react-icons/im';
             };
 
      
-        const [isScheduling, setIsScheduling] = useState(false);
-        const [isScheduled, setIsScheduled] = useState(false);
-        const [scheduleErr, setScheduleErr] = useState('');
+       
         
         const [show, setShow] = useState(false);
         
         const [value, setValue] = useState();
+        
+        const [validated, setValidated] = useState(false);
+
+        const handleSubmit = (event) => {
+          const form = event.currentTarget;
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+      
+          setValidated(true);
+        };
 
   return (
     <>
@@ -51,29 +57,30 @@ import { ImEarth } from 'react-icons/im';
                 <h3>Select Date and Time</h3>
                
                     <div className="timezonediv">
-                        <div style={{marginRight:"5px", marginLeft:"5px"}}>
-                            <p style={{color:"gray"}}><ImEarth /></p>
+                        <div className="timezone--wrapper">
+                        <TimezoneSelect
+                            value={selectedTimezone}
+                            onChange={setSelectedTimezone}
+                            labelStyle='altName'
+                            timezones={{
+                                ...allTimezones,
+                                'America/Lima': 'Pittsburgh',
+                                'Europe/Berlin': 'Frankfurt',
+                            }}
+                            />
                         </div>
-                    <SelectTimezone
-                        id="custom_timezone"
-                        name={"Custom timezone"}
-                        label=  "Time zone" labelStyles={{fontWeight:"bold"}}
-                        value={value}
-                        onChange={({ label, value }) => setValue(value)}
-                        defaultToSystemTimezone
-                        containerStyles={{ width: 300 }}
-                        optionLabelFormat={(timzone) =>
-                            `${timzone.name} (${timzone.abbreviation})`
-                        }
-                    />
+                        
+                    
                     </div>
+                    {/* <TimezonePicker  
+                     absolute      = {false}  
+                      defaultValue  = "Europe/Moscow" 
+                        placeholder   = "Select timezone..."   onChange      = {this.handleChange} /> */}
+
 
                  <div style={{paddingLeft:"0.6rem"}}>
                     <DayTimePicker
                     timeSlotSizeMinutes={30}
-                    isLoading={isScheduling}
-                    isDone={isScheduled}
-                    err={scheduleErr}
                     onConfirm={handleScheduled}
                     confirmText={"Book Appointment"} onConfirm={() => setShow(true)}
                    
@@ -95,56 +102,112 @@ import { ImEarth } from 'react-icons/im';
                     </Modal.Header>
                     <Modal.Body className='form-body'>
                     <div>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Name"
-                        className="mb-3"
-                    >
-                        <Form.Control type="name" placeholder="name@example.com" />
-                    </FloatingLabel>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Email"
-                        className="mb-3"
-                    >
-                        <Form.Control type="email" placeholder="name@example.com" />
-                    </FloatingLabel>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Phone"
-                        className="mb-3"
-                    >
-                        <Form.Control type="phone" placeholder="name@example.com" />
-                    </FloatingLabel>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Company name (Optional)"
-                        className="mb-3"
-                    >
-                        <Form.Control type="name" placeholder="name@example.com" />
-                    </FloatingLabel>
-                    <FloatingLabel 
-                        controlId="floatingTextarea2"
-                        label="Message">
-                        <Form.Control
-                        as="textarea"
-                        placeholder="Leave a comment here"
-                        className='message-box'
-                        />
-                    </FloatingLabel>
-                    <Form.Group className='terms-check'>
-                        <Form.Check
-                        required
-                        label="Agree to terms and conditions" style={{fontWeight:"100"}}
-                        feedback="You must agree before submitting."
-                        feedbackType="invalid"
-                        className='form-label' 
-                        />
-                    </Form.Group>
-                    <div style={{textAlign:"center", marginTop:"1rem"}}>
-                        <button className='submit-btn'>Submit</button>
-                    </div>
+                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                        <Form.Group  controlId="validationCustom01">
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    label="Full Name"
+                                    className="mb-3"
+                                >
+                                <Form.Control 
+                                required
+                                type="text"
+                                 placeholder="Full Name"
+                                
+                                  />
+                                  <Form.Control.Feedback type="invalid">
+                                    Required.
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                             </Form.Group>  
+
+                             <Form.Group  controlId="validationCustomUsername">
+                             <InputGroup hasValidation>
+                            
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    label="Email"
+                                    className="mb-3"
+                                >
+                                    <Form.Control 
+                                    type="text"
+                                    placeholder="name@example.com"
+                                    aria-describedby="inputGroupPrepend"
+                                    required
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        Required.
+                                        </Form.Control.Feedback>
+                                </FloatingLabel>
+                            </InputGroup>
+                            </Form.Group>
+
+                            <Form.Group  controlId="validationCustom03">
+                                <FloatingLabel
+                                    controlId="floatingInput"
+                                    label="Phone"
+                                    className="mb-3"
+                                >
+                                    <Form.Control 
+                                    required
+                                    type="number"
+                                    placeholder="Phone"
+                                    />
+                                     <Form.Control.Feedback type="invalid">
+                                     Required.
+                                    </Form.Control.Feedback>
+                                </FloatingLabel>
+                                </Form.Group>
+
+                                <Form.Group controlId="validationCustom04">
+                                    <FloatingLabel
+                                        controlId="floatingInput"
+                                        label="Company name (Optional)"
+                                        className="mb-3"
+                                    >
+                                        <Form.Control 
+                                        required
+                                        type="text" 
+                                        placeholder="Company name"
+                                        />
+                                         <Form.Control.Feedback type="invalid">
+                                         Required.
+                                        </Form.Control.Feedback>
+                                    </FloatingLabel>
+                                    </Form.Group>
+
+                                    <Form.Group controlId="validationCustom05">
+                                    <FloatingLabel 
+                                        controlId="floatingTextarea2"
+                                        label="Message">
+                                        <Form.Control
+                                        required
+                                        as="textarea"
+                                        placeholder="Leave a comment here"
+                                        className='message-box'
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                        Required.
+                                        </Form.Control.Feedback>
+                                    </FloatingLabel>
+                                    </Form.Group>
+
+                                    <Form.Group className='terms-check'>
+                                        <Form.Check
+                                        required
+                                        label="Agree to terms and conditions" style={{fontWeight:"100"}}
+                                        feedback="You must agree before submitting."
+                                        feedbackType="invalid"
+                                        className='form-label' 
+                                        />
+                                    </Form.Group>
+
+                                    <div style={{textAlign:"center", marginTop:"1rem"}}>
+                                        <button className='submit-btn'>Submit</button>
+                                    </div>
                         
+                  
+                    </Form>
                     </div>
                     </Modal.Body>
                 </Modal>
