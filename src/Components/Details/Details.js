1072b546d707c9d4a1f3625bf1793d7cec4e8ef1
Ranjana <Ranjana.neutroline@gmail.com>
2023-06-21 18:41:55 +0545

    21stjune

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -7,26 +7,29 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { BsTelephoneFill } from 'react-icons/bs';
 import DayTimePicker from '@mooncake-dev/react-day-time-picker';
-import TimezoneSelect, { allTimezones } from 'react-timezone-select'
-import spacetime from "spacetime";
 import Modal from 'react-bootstrap/Modal';
 import { Controller, useForm } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Select from "react-select";
 import { TextField, Button, ButtonGroup } from "@mui/material";
 import Submission from "./Submission";
+import TimezoneSelect from "react-timezone-select"
+import Calendar from 'react-calendar';
+import Time from './Time.js'
+
 
 
  function Details() {
     // const [value, onChange] = useState(new Date());
 
-    const [selectedTimezone, setSelectedTimezone] =useState(
-        Intl.DateTimeFormat().resolvedOptions().timeZone
-      )
+    // const [selectedTimezone, setSelectedTimezone] =useState(
+    //     Intl.DateTimeFormat().resolvedOptions().timeZone
+    //   )
 
-        const [date, setDate] = useState(new Date());
+    const [selectedTimezone, setSelectedTimezone] = useState({})
+
+        // const [date, setDate] = useState(new Date());
         
         const handleScheduled = dateTime => {
         console.log('scheduled: ', dateTime);
@@ -41,29 +44,13 @@ import Submission from "./Submission";
         
        
         
-        const {
-            register,
-            handleSubmit,
-            reset,
-            resetField,
-            control,
         
-            formState: { errors },
-          } = useForm();
-          const onSubmit = (data, e) => {
-            console.log(data);
-            reset();
-            resetField();
-            // const token = captchaRef.current.getValue();
-            // captchaRef.current.reset();
-            // swal("Form has been Submitted");
-            toast.success("Success\n your form has been submitted");
-          };
-          const onCaptchaComplete = (response) => {
-            console.log(response);
-          };
 
           
+          const [date, setDate] = useState(new Date());
+          const [showTime, setShowTime] = useState(false)
+
+         
 
   return (
     <>
@@ -75,246 +62,43 @@ import Submission from "./Submission";
                
                     <div className="timezonediv">
                         <div className="timezone--wrapper">
-                        <TimezoneSelect
-                            value={selectedTimezone}
-                            onChange={setSelectedTimezone}
-                            labelStyle='altName'
-                            timezones={{
-                                ...allTimezones,
-                                'America/Lima': 'Pittsburgh',
-                                'Europe/Berlin': 'Frankfurt',
-                            }}
-                            />
+                            <div className="select-wrapper" >
+                              <TimezoneSelect
+                                sx={{height: "5px"}}
+                                value={selectedTimezone}
+                                onChange={setSelectedTimezone}
+                                placeholder= {"Select Time zone"}
+                               
+                              />
+                            </div>
                         </div>
-                        
+                      </div>
+                  
+                     
+                <div style={{paddingLeft:"0.6rem"}}>
+               
+                  <div className="calendar-container">
+                    <Calendar onChange={setDate} value={date} onClickDay={() => setShowTime(true)}/>
+                    { showTime ? <Time showTime={showTime} date={date} value={date}/> : null }
                     
-                    </div>
-                    {/* <TimezonePicker  
-                     absolute      = {false}  
-                      defaultValue  = "Europe/Moscow" 
-                        placeholder   = "Select timezone..."   onChange      = {this.handleChange} /> */}
-
-
-                 <div style={{paddingLeft:"0.6rem"}}>
-                 
-                    
-                    
-                    <DayTimePicker 
+                  </div>
+                
+                    {/* <DayTimePicker 
                     timeSlotSizeMinutes={30}
-                    onConfirm={handleScheduled}
                     confirmText={"Book Appointment"} onConfirm={() => setShow(true)}
                    
-                     />
-                    
+                     /> */}
+                
+                
+                 
+                 
                    
                 </div>  
-                 
-                <Modal
-                    show={show}
-                    onHide={() => setShow(false)}
-                    dialogClassName="modal-90w"
-                    aria-labelledby="example-custom-modal-styling-title"
-                >
-                    <Modal.Header closeButton className='header-form'>
-                    <Modal.Title id="example-custom-modal-styling-title">
-                        Enter Details
-                    </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body className='form-body'>
-                    <div>
-                    <div className="services-form-container">
-      {" "}
-      <div className="mainForm">
-        <ToastContainer position="top-center" />
-        <form className="form_container" onSubmit={handleSubmit(onSubmit)}>
-          <Row
-            className="form-row m-0"
-            style={{ height: "auto", width: "auto" }}
-          >
-            <Col sm={12} lg={12} md={12} className="form-col ">
-
-                <TextField
-                  // required
-
-                  fullWidth
-                  label="Full Name"
-                  margin="dense"
-                  name="name"
-                  variant="filled"
-                  placeholder="Full Name"
-                  // value={myForm.values.name}
-                  // onChange={myForm.handleChange}
-                  // error={!!myForm.errors.companyName}
-                  // helperText={myForm.errors.name}
-                  autoComplete="off"
-                  sx={{
-                    "& .MuiFilledInput-underline:before": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:after": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
-                      {
-                        borderBottom: "none",
-                      },
-                  }}
-                  {...register("name", { required: "Required", minLength: 3 })}
-                  error={!!errors?.name}
-                  // helperText={errors?.name ? errors.name.message : null}
-                />
-
-                <TextField
-                  // required
-                  fullWidth
-                  label="Email"
-                  margin="dense"
-                  name="email"
-                  variant="filled"
-                  placeholder="Email"
-                  autoComplete="off"
-                  sx={{
-                    "& .MuiFilledInput-underline:before": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:after": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
-                      {
-                        borderBottom: "none",
-                      },
-                  }}
-                  {...register("email", {
-                    required: "Required field",
-                    pattern: {
-                      value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
-                      message: "Invalid email address",
-                    },
-                  })}
-                  error={!!errors?.email}
-                />
-               
-                  
-                  <TextField
-                    // inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
-
-                    fullWidth
-                    label="Phone"
-                    margin="dense"
-                    name="phone"
-                    autoComplete="off"
-                    variant="filled"
-                    placeholder=" Landline/Mobile Number"
-                    type="number"
-                    className="txtfield_phone "
-                    sx={{
-                      "& .MuiFilledInput-underline:before": {
-                        borderBottom: "none",
-                      },
-                      "& .MuiFilledInput-underline:after": {
-                        borderBottom: "none",
-                      },
-                      "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
-                        {
-                          borderBottom: "none",
-                        },
-                    }}
-                    {...register("phone", { required: "Required" })}
-                    error={!!errors?.phone}
-                  />
                 
-                <TextField
-                fullWidth
-                  id="outlined-basic"
-                  label="Company name (Optional)"
-                  variant="filled"
-                  size="small"
-                  margin="dense"
-                  name="CompanyName"
-                  placeholder="Company name"
-                  autoComplete="off"
-                  sx={{
-                    "& .MuiFilledInput-underline:before": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:after": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
-                      {
-                        borderBottom: "none",
-                      },
-                  }}
-                  {...register("CompanyName", { required: "Required" })}
-                  error={!!errors?.CompanyName}
-                />
-               
                 
-                <TextField
-                  fullWidth
-                  multiline
-                  rows={3}
-                  label="Message"
-                  margin="dense"
-                  name="message"
-                  variant="filled"
-                  placeholder="Your Message"
-                  // value={myForm.values.message}
-                  // onChange={myForm.handleChange}
-                  // error={!!myForm.errors.companyName}
-                  // helperText={myForm.errors.message}
-                  autoComplete="off"
-                  sx={{
-                    "& .MuiFilledInput-underline:before": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:after": {
-                      borderBottom: "none",
-                    },
-                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
-                      {
-                        borderBottom: "none",
-                      },
-                  }}
-                  {...register("message", { required: "Required" })}
-                  error={!!errors?.message}
-                />
-              
-                    
-                   
-               
-                <div className="buttonsubmit">
-                  <Button
-                    className='submit-btn'
-                   
-                    // disabled={!myForm.isValid}
-                    // onClick={myForm.submitForm}
-                    type="submit"
-                    variant="contained"
-                  >
-                    Submit
-                  </Button>
-                </div>
-              
-            </Col>
-          </Row>
-        </form>
-      </div>
-    </div>
-                    </div>
-                    </Modal.Body>
-                </Modal>
                             
                 
                    
-                    
-             
-            
-
-                   
-           
-               
             </Col>
 
             <Col className='detail-right'>
@@ -344,6 +128,8 @@ import Submission from "./Submission";
             </Row>
             
            
+           
+
 
         </Container>
         
---
+++ src/Components/Details/Time.js
@@ -0,0 +1,306 @@
+import React from 'react'
+import {useState} from 'react';
+import Calendar from 'react-calendar';
+import Modal from 'react-bootstrap/Modal';
+import { Controller, useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { TextField, Button, ButtonGroup } from "@mui/material";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { MdDateRange } from 'react-icons/md';
+
+
+const time = ['09:00','09:30','10:00','10:30','11:00','11:30','12:00','13:30','14:00','14:30',
+'15:00','16:00']
+
+
+function Times(props) {
+
+ const [event, setEvent] = useState(null)
+ const [info, setInfo] = useState(false)
+ const [book, setBook] = useState(false)
+
+ const [date, setDate] = useState(new Date());
+
+ const [show, setShow] = useState(false);
+
+ const {
+  register,
+  handleSubmit,
+  reset,
+  resetField,
+  control,
+
+  formState: { errors },
+} = useForm();
+const onSubmit = (data, e) => {
+  console.log(data);
+  reset();
+  resetField();
+  // const token = captchaRef.current.getValue();
+  // captchaRef.current.reset();
+  // swal("Form has been Submitted");
+  toast.success("Success\n your form has been submitted");
+};
+const onCaptchaComplete = (response) => {
+  console.log(response);
+};
+
+ function displayInfo(e) {
+   setInfo(true);
+   setBook(true);
+   setEvent(e.target.innerText);
+}
+
+return (
+ 
+ <div className="times">
+  <div className='date-div'>
+  {date.length > 0 ? (
+    
+              <p>
+                <span>Start:</span>
+                {date[0].toDateString()}
+                &nbsp;
+                &nbsp;
+                <span>End:</span>{date[1].toDateString()}
+              </p>
+                      ) : (
+              <p className='date-p'><MdDateRange/>&nbsp;&nbsp;
+                  {date.toDateString()}
+              </p> 
+      
+                      )
+              } 
+      </div>
+
+      <div className='timeslot-div'>
+   {time.map(times => {
+    return (
+    <div className="space">
+       <button onClick={(e)=> displayInfo(e)} className='time-btn'> {times} </button>
+     
+    </div>
+    
+        )
+     })}
+
+     </div>
+    <div className='book-div'>
+      {info ? `Your appointment is set to ${event} ${props.date.toDateString()}` : null}
+      <div className='bookbtn-div'>
+      { info ? <Button onClick={() => setShow(true)} className='bookbtn'>Book Appointment</Button> : null }
+      </div>
+     
+    </div>
+    <Modal
+                    show={show}
+                    onHide={() => setShow(false)}
+                    dialogClassName="modal-90w"
+                    aria-labelledby="example-custom-modal-styling-title"
+                >
+                    <Modal.Header closeButton className='header-form'>
+                    <Modal.Title id="example-custom-modal-styling-title">
+                        Enter Details
+                    </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className='form-body'>
+                    <div>
+                    <div className="services-form-container">
+                      {" "}
+                      <div className="mainForm">
+                        <ToastContainer position="top-center" />
+                        <form className="form_container" onSubmit={handleSubmit(onSubmit)}>
+                          <Row
+                            className="form-row m-0"
+                            style={{ height: "auto", width: "auto" }}
+                          >
+                            <Col sm={12} lg={12} md={12} className="form-col ">
+
+                                <TextField
+                                  // required
+
+                                  fullWidth
+                                  label="Full Name"
+                                  margin="dense"
+                                  name="name"
+                                  variant="filled"
+                                  // placeholder="Full Name"
+                                  // value={myForm.values.name}
+                                  // onChange={myForm.handleChange}
+                                  // error={!!myForm.errors.companyName}
+                                  // helperText={myForm.errors.name}
+                                  autoComplete="off"
+                                  sx={{
+                                    "& .MuiFilledInput-underline:before": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:after": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
+                                      {
+                                        borderBottom: "none",
+                                      },
+                                  }}
+                                  {...register("name", { required: "Required", minLength: 3 })}
+                                  error={!!errors?.name}
+                                  // helperText={errors?.name ? errors.name.message : null}
+                                />
+
+                                <TextField
+                                  // required
+                                  fullWidth
+                                  label="Email"
+                                  margin="dense"
+                                  name="email"
+                                  variant="filled"
+                                  // placeholder="Email"
+                                  autoComplete="off"
+                                  sx={{
+                                    "& .MuiFilledInput-underline:before": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:after": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
+                                      {
+                                        borderBottom: "none",
+                                      },
+                                  }}
+                                  {...register("email", {
+                                    required: "Required field",
+                                    pattern: {
+                                      value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+                                      message: "Invalid email address",
+                                    },
+                                  })}
+                                  error={!!errors?.email}
+                                />
+                              
+                                  
+                                  <TextField
+                                    // inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+
+                                    fullWidth
+                                    label="Phone"
+                                    margin="dense"
+                                    name="phone"
+                                    autoComplete="off"
+                                    variant="filled"
+                                    placeholder=" Landline/Mobile Number"
+                                    type="number"
+                                    className="txtfield_phone "
+                                    sx={{
+                                      "& .MuiFilledInput-underline:before": {
+                                        borderBottom: "none",
+                                      },
+                                      "& .MuiFilledInput-underline:after": {
+                                        borderBottom: "none",
+                                      },
+                                      "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
+                                        {
+                                          borderBottom: "none",
+                                        },
+                                    }}
+                                    {...register("phone", { required: "Required" })}
+                                    error={!!errors?.phone}
+                                  />
+                                
+                                <TextField
+                                fullWidth
+                                  id="outlined-basic"
+                                  label="Company name (Optional)"
+                                  variant="filled"
+                                  size="small"
+                                  margin="dense"
+                                  name="CompanyName"
+                                  placeholder="Company name"
+                                  autoComplete="off"
+                                  sx={{
+                                    "& .MuiFilledInput-underline:before": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:after": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
+                                      {
+                                        borderBottom: "none",
+                                      },
+                                  }}
+                                  // {...register("CompanyName", { required: "Required" })}
+                                  // error={!!errors?.CompanyName}
+                                />
+                              
+                                
+                                <TextField
+                                  fullWidth
+                                  multiline
+                                  rows={3}
+                                  label="Message"
+                                  margin="dense"
+                                  name="message"
+                                  variant="filled"
+                                  placeholder="Your Message"
+                                  // value={myForm.values.message}
+                                  // onChange={myForm.handleChange}
+                                  // error={!!myForm.errors.companyName}
+                                  // helperText={myForm.errors.message}
+                                  autoComplete="off"
+                                  sx={{
+                                    "& .MuiFilledInput-underline:before": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:after": {
+                                      borderBottom: "none",
+                                    },
+                                    "& .MuiFilledInput-underline:hover:not(.Mui-disabled):before":
+                                      {
+                                        borderBottom: "none",
+                                      },
+                                  }}
+                                  // {...register("message", { required: "Required" })}
+                                  // error={!!errors?.message}
+                                />
+                               {/* <Form.Group className="mb-3 mt-1">
+                                  <Form.Check
+                                    required
+                                    label="Agree to terms and conditions"
+                                    feedback="You must agree before submitting."
+                                    feedbackType="invalid"
+                                    className='agree-term'
+                                  />
+                                </Form.Group> */}
+                                    
+                                   <div className="buttonsubmit">
+                                  <Button
+                                    className='submit-btn'
+                                  
+                                    // disabled={!myForm.isValid}
+                                    // onClick={myForm.submitForm}
+                                    type="submit"
+                                    variant="contained"
+                                  >
+                                    Submit
+                                  </Button>
+                                </div>
+                              
+                            </Col>
+                          </Row>
+                        </form>
+                      </div>
+                    </div>
+                    </div>
+                    </Modal.Body>
+                </Modal>
+
+    
+ </div>
+  )
+}
+
+export default Times;
